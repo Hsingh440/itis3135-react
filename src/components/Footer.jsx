e95a52af0533523940bd77b93506c98564dbd35a
Hsingh440 <hsingh19@uncc.edu>
2025-11-12 18:55:31 -0500

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,21 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>&copy; {2025} Harsimran Singh - ITIS 3135</p>
-      {/* add your validator links here if required */}
+     <nav>
+       <a href="https://webpages.charlotte.edu/hsingh19">CLT Web</a> 
+                ||
+                <a href="https://github.com/Hsingh440/">GitHub</a> 
+                ||
+                <a href="https://hsingh440.github.io/">GitHub.io</a> 
+                ||
+                <a href="https://www.freecodecamp.org/hsingh19">freeCodeCamp</a>
+                ||
+                <a href="https://www.codecademy.com/profiles/hsingh19charlotte.edu">Codecademy</a>
+                ||
+                <a href="https://www.linkedin.com/in/harry11d/">LinkedIn</a> 
+
+        </nav>
+    <p>  Page Built by  <a href="singhsoftware.com/">Singh Software Enterprises</a> &copy; 2025</p>
     </footer>
   );
 }
