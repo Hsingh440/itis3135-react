72e8561efa8568317d379778c9e1f1d6dddde755
Hsingh440 <hsingh19@uncc.edu>
2025-11-12 18:58:00 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Introduction" element={<Introduction />} />
-        <Route path="/Contract" element={<Contract />} />
+        <Route path="/pages/Introduction" element={<Introduction />} />
+        <Route path="/pages/Contract" element={<Contract />} />
         {/* Optional: Add a catch-all for 404 pages */}
         {/* <Route path="*" element={<h2>404 Not Found</h2>} /> */}
       </Routes>
